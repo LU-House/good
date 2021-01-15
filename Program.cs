cc3228b0a90591beed88885b86cc0dbc24d29269
Zeyu Lu <luzeyu3077@gmail.com>
2021-01-16 01:38:56 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace good
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What the fuck is it");
+            
+        }
+    }
+}
